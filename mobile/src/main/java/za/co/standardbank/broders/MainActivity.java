461c12b6003e38ecbed42df7c9027f6e74064f98
stuey86 <stuart.brande@gmail.com>
2015-07-06 20:30:56 +0200

    Ignore

--- mobile/src/main/java/za/co/standardbank/broders/MainActivity.java
+++ mobile/src/main/java/za/co/standardbank/broders/MainActivity.java
@@ -3,9 +3,12 @@ package za.co.standardbank.broders;
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
+import android.text.TextUtils;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.AutoCompleteTextView;
+import android.widget.EditText;
 
 import za.co.standardbank.broders.messaging.NotificationHubHelper;
 import za.co.standardbank.broders.property.PropertyDetailsActivity;
@@ -13,6 +16,9 @@ import za.co.standardbank.broders.scanning.ScanActivity;
 
 
 public class MainActivity extends Activity {
+
+    private AutoCompleteTextView mEmailView;
+    private EditText mPasswordView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -30,5 +36,64 @@ public class MainActivity extends Activity {
     public void login(View view) {
         Intent propertyDetailsIntent = new Intent(this, PropertyDetailsActivity.class);
         startActivity(propertyDetailsIntent);
+    }
+
+    public void attemptLogin() {
+//        if (mAuthTask != null) {
+//            return;
+//        }
+
+        String email = mEmailView.getText().toString();
+        String password = mPasswordView.getText().toString();
+
+        if (validateForm(email, password)) {
+//            showProgress(true);
+//            mAuthTask = new UserLoginTask(email, password);
+//            mAuthTask.execute((Void) null);
+        }
+    }
+
+    private boolean isEmailValid(String email) {
+        return email.contains("@");
+    }
+
+    private boolean isPasswordValid(String password) {
+        return password.length() > 4;
+    }
+
+    public boolean validateForm(String email, String password) {
+        // Reset errors.
+        mEmailView.setError(null);
+        mPasswordView.setError(null);
+
+        boolean cancel = false;
+        View focusView = null;
+
+
+        // Check for a valid password, if the user entered one.
+        if (!TextUtils.isEmpty(password) && !isPasswordValid(password)) {
+            mPasswordView.setError(getString(R.string.error_invalid_password));
+            focusView = mPasswordView;
+            cancel = true;
+        }
+
+        // Check for a valid email address.
+        if (TextUtils.isEmpty(email)) {
+            mEmailView.setError(getString(R.string.error_field_required));
+            focusView = mEmailView;
+            cancel = true;
+        } else if (!isEmailValid(email)) {
+            mEmailView.setError(getString(R.string.error_invalid_email));
+            focusView = mEmailView;
+            cancel = true;
+        }
+
+        if (cancel) {
+            // There was an error; don't attempt login and focus the first
+            // form field with an error.
+            focusView.requestFocus();
+        }
+
+        return !cancel;
     }
 }
---
+++ mobile/src/main/java/za/co/standardbank/broders/login/LoginActivity.java
@@ -0,0 +1,17 @@
+package za.co.standardbank.broders.login;
+
+import android.support.v7.app.ActionBarActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+
+import za.co.standardbank.broders.R;
+
+public class LoginActivity extends ActionBarActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+    }
+}
